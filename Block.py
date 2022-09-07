199fb7c67324c2adcdbdeb9459f9c330531b13b4
Stephan Klein <stephan.klein.at@gmail.com>
2022-09-07 19:40:21 +0200

    Add Missing Requests

--- Block.py
+++ Block.py
@@ -21,20 +21,33 @@ class Block:
             return self.hash
         else:
             return ""
+    
+    def get_tx_by_hash(self, hash):
+        for tx in self.transactions:
+            if tx.get_hash() == hash:
+                return tx
 
     def get_transaction_hashes(self):
         return [x.get_hash() for x in self.transactions]
+
+    def serial_transactions(self):
+        return list(map(lambda x: {
+                "utxos": x["utxos"] if "utxos" in x else [],
+                "receiver_public_keys": x["receiver_public_keys"],
+                "messages": x["messages"],
+                "signature": x["signature"] if "signature" in x else "",
+            }, list(map(lambda x: x.get_dict(), self.transactions))))
+    
+    def get_transactions(self):
+         return {
+           "transactions": self.serial_transactions()
+        }
 
     def get_dict(self):
         return {
            "hash": self.get_hash(),
            "transaction_hashes": self.get_transaction_hashes(),
-           "transactions": list(map(lambda x: {
-                "utxos": x["utxos"] if "utxos" in x else [],
-                "receiver_public_keys": x["receiver_public_keys"],
-                "messages": x["messages"],
-                "signature": x["signature"] if "signature" in x else "",
-            }, list(map(lambda x: x.get_dict(), self.transactions)))),
+           "transactions": self.serial_transactions(),
             "hash_previous_block": self.hash_previous_block,
             "nonce": self.nonce
         }
--- Server.py
+++ Server.py
@@ -58,6 +58,38 @@ def getBlock(blockhash):
       return repr(e)
   return "Wrong HTTP method"
 
+@app.route('/block/<blockhash>/tx', methods=['GET'])
+def getTxs(blockhash):
+  if request.method == 'GET':
+    try:
+      block = get_blockchain().get_block_by_hash(blockhash)
+      if block == None:
+        return repr("Error: No block found with this hash")
+      else:
+        return Response(json.dumps(block.get_transactions()), mimetype='text/json')
+    except Exception as e:
+      logging.error(e,exc_info=True)
+      return repr(e)
+  return "Wrong HTTP method"
+
+@app.route('/block/<blockhash>/tx/<txhash>', methods=['GET'])
+def getTx(blockhash,txhash):
+  if request.method == 'GET':
+    try:
+      block = get_blockchain().get_block_by_hash(blockhash)
+      if block == None:
+        return repr("Error: No block found with this hash")
+      else:
+        tx = block.get_tx_by_hash(txhash)
+        if tx == None:
+          return repr("Error: No tx found with this hash")
+        else:
+          return Response(json.dumps(tx.get_dict()), mimetype='text/json')
+    except Exception as e:
+      logging.error(e,exc_info=True)
+      return repr(e)
+  return "Wrong HTTP method"
+
 def init_server():
   logging.basicConfig(level=logging.INFO)
   logging.info("Team 4 Blockchain starting up")
