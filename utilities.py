add081c3a9bb59372fc0021624e1e9e31f818438
Stephan Klein <stephan.klein.at@gmail.com>
2022-09-07 12:51:14 +0200

    isInstance does not work here on JSON deserialized object

--- utilities.py
+++ utilities.py
@@ -10,19 +10,20 @@ def serialize_utxo(utxo):
   )
 
 def serialize_transaction(tx):
-    if isinstance(tx, Transaction):
-        return Transaction(
-            utxos=list(map(lambda u: serialize_utxo(u), tx["utxos"] if "utxos" in tx else [])),
-            receiver_public_keys=tx["receiver_public_keys"] if "receiver_public_keys" in tx else [],
-            messages=tx["messages"] if "messages" in tx else [],
-            signature=tx["signature"] if "signature" in tx else "",
-        )
-    if isinstance(tx, UnsignedTransaction):
-      return UnsignedTransaction(
-            utxos=list(map(lambda u: serialize_utxo(u), tx["utxos"] if "utxos" in tx else [])),
-            receiver_public_keys=tx["receiver_public_keys"] if "receiver_public_keys" in tx else [],
-            messages=tx["messages"] if "messages" in tx else [],
-      )
+    if "utxos" in tx:
+        if "signature" in tx:
+            return Transaction(
+                utxos=list(map(lambda u: serialize_utxo(u), tx["utxos"] if "utxos" in tx else [])),
+                receiver_public_keys=tx["receiver_public_keys"] if "receiver_public_keys" in tx else [],
+                messages=tx["messages"] if "messages" in tx else [],
+                signature=tx["signature"] if "signature" in tx else "",
+            )
+        else:
+            return UnsignedTransaction(
+                    utxos=list(map(lambda u: serialize_utxo(u), tx["utxos"] if "utxos" in tx else [])),
+                    receiver_public_keys=tx["receiver_public_keys"] if "receiver_public_keys" in tx else [],
+                    messages=tx["messages"] if "messages" in tx else [],
+            )
 
     return Coinbase(receiver=tx["receiver_public_keys"][0])
 
